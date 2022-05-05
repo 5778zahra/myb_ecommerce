026c13767982b39f143b49c88cf5abaf364df22a
Hammani Zahra <dekzahra@outlook.fr>
2022-05-05 16:25:55 +0200

    rollerworks password strength validator

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Rollerworks\Component\PasswordStrength\Validator\Constraints\PasswordStrength;
 
 class RegistrationFormType extends AbstractType
 {
@@ -55,33 +56,32 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'type' => PasswordType::class,
-                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
                 'mapped' => false,
                 'required' => true,
                 'label' =>'Votre mot de passe',
                 'first_options' => [ 
                     'label' => 'Mot de passe',
-                'attr' => [
-                    'placeholder' =>'Merci de saisir votre mot de passe'
-                ]
-            ],
+                    'attr' => [
+                        'placeholder' =>'Merci de saisir votre mot de passe'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Merci de renseigner un mot de passe',
+                        ]),
+                        new PasswordStrength([
+                            'minLength' => 8,
+                            'tooShortMessage' => 'Le mot de passe doit contenir au moins {{length}} caractères',
+                            'minStrength' => 4,
+                            'message' => 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial'
+                        ])
+                    ]
+                ],
                 'second_options' => [
                     'label' => 'Confirmez votre mot de passe'],
                 'attr' => [
                     'placeholder' => 'Merci de confirmer votre mot de passe',
                     'autocomplete' => 'new-password'],
-                    
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.'
             ])
             // ->add('password', RepeatedType::class, [
             //     'type' => PasswordType::Class,
